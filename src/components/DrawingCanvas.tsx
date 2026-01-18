d12766630e05fe6d39d1103e63bf9729493356dc
Thousands Of Ties <thousands.of.ties@gmail.com>
2026-01-18 17:02:23 +0900

    fix: Disable distance filter to prevent chords, restore user color

--- src/components/DrawingCanvas.tsx
+++ src/components/DrawingCanvas.tsx
@@ -87,7 +87,7 @@ export const DrawingCanvas = React.forwardRef<HTMLCanvasElement, DrawingCanvasPr
         stopDrawing: hookStopDrawing
     } = useDrawing(canvasRef, {
         width: size,
-        color: '#0000FF', // FORCE BLUE for Debugging (User Request)
+        color,
         onPathComplete: (path) => {
             // なげなわ選択が有効で、ループとして認識された場合はパスを追加しない
             if (onLassoComplete && onLassoComplete(path)) {
